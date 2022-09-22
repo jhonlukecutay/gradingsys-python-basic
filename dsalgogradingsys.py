b1abd76f0b5ddc000805e27df711e4d7988095b0
Jhon Luke Cutay <113082100+jhonlukecutay@users.noreply.github.com>
2022-09-22 19:19:27 +0800

    Add files via upload

--- dsalgogradingsys.py
+++ dsalgogradingsys.py
@@ -12,10 +12,10 @@ recitation1 = int(input("Please enter the score of the recitation: "))
 exam1 = int(input("Please enter the score of the exam: "))
 
 #grade system.
-quizTotal = quiz1 + quiz2 + quiz3 ; quizDec = quizTotal / 30 ; quizPnts = quizDec * 100
-reciDec = recitation1 / 100 ; reciPnts = reciDec * 100
-examDec = exam1 / 50 ; examPnts = examDec * 100
-totalAve = quizPnts + reciPnts + examPnts ; totalPerc = totalAve / 300
+quizTotal = quiz1 + quiz2 + quiz3 ; quizDec = quizTotal / 30 ; quizPnts = quizDec * 30
+reciDec = recitation1 / 100 ; reciPnts = reciDec * 30
+examDec = exam1 / 50 ; examPnts = examDec * 40
+totalAve = quizPnts + reciPnts + examPnts ; totalPerc = totalAve / 100
 totalAvePerc = "{:.2%}".format(totalPerc)
 print(totalAvePerc)
 
